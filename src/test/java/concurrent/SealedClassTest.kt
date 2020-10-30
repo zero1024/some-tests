9f4404d48ca02dac6205caec24f4c61e054a6b70
oleg1024 <o.perekhod@profeelab.net>
2020-10-30 17:25:44 +0300

    sealed class test

---
+++ src/test/java/concurrent/SealedClassTest.kt
@@ -0,0 +1,22 @@
+package concurrent
+
+import org.junit.Test
+
+class SealedClassTest {
+
+    @Test
+    fun test1() {
+        fun printGender(gender: Gender) {
+            when (gender) {
+                is Female -> println("Female")
+                is Male -> println("Male")
+            }
+        }
+        printGender(Female)
+        printGender(Male(dickSize = 15))
+    }
+}
+
+sealed class Gender
+data class Male(val dickSize: Int) : Gender()
+object Female : Gender()
