d49dc5f69c798f47134488c2b0a21308871c4b73
oleg1024 <o.perekhod@profeelab.net>
2020-12-26 18:31:52 +0300

    java14 records

--- src/test/java/feature/java/Java14.java
+++ src/test/java/feature/java/Java14.java
@@ -37,4 +37,20 @@ public class Java14 {
                 third""";
         assertThat(s).isEqualTo("first\nsecond third");
     }
+
+    @Test
+    void testRecord() {
+        Person person1 = new Person("Oleg", "Perekhod");
+        Person person2 = new Person("Oleg", "Perekhod");
+        Person person3 = new Person("Anton", "Perekhod");
+
+        assertThat(person1).isEqualTo(person2);
+        assertThat(person1).isNotEqualTo(person3);
+    }
+
+    private static record Person(String name, String lastName) {
+
+    }
+
+
 }
