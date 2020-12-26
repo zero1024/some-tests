ad2e3aa589a37160c3a3b3569539208a681a8e5c
oleg1024 <o.perekhod@profeelab.net>
2020-12-26 19:24:29 +0300

    more memory tests

--- src/test/java/feature/java/GCTest.java
+++ src/test/java/feature/java/GCTest.java
@@ -2,7 +2,10 @@ package feature.java;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.management.ManagementFactory;
 import java.util.HashMap;
+
+import static org.apache.commons.io.FileUtils.byteCountToDisplaySize;
 
 public class GCTest {
 
@@ -19,5 +22,16 @@ public class GCTest {
             new HashMap<>().put(1, 2);
         }
         System.gc();
+    }
+
+    /**
+     * -Xmx512m -Xms128m
+     */
+    @Test
+    void testDefaultXmx() {
+        var memoryMXBean = ManagementFactory.getMemoryMXBean();
+        System.out.println("Init:" + byteCountToDisplaySize(memoryMXBean.getHeapMemoryUsage().getInit()));
+        System.out.println("Used:" + byteCountToDisplaySize(memoryMXBean.getHeapMemoryUsage().getUsed()));
+        System.out.println(("Max:" + byteCountToDisplaySize(memoryMXBean.getHeapMemoryUsage().getMax())));
     }
 }
