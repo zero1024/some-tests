e968757430465da661daea298701c25466869140
oleg1024 <o.perekhod@profeelab.net>
2021-09-08 18:38:26 +0300

    update kotlin to 1.5.21 and test ApplicationRunner

--- spring-boot-test/src/main/java/ru/SpringBootTestApplication.java
+++ spring-boot-test/src/main/java/ru/SpringBootTestApplication.java
@@ -1,13 +1,20 @@
 package ru;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner testApplicationRunner() {
+        return (args) -> System.out.println("ApplicationRunner test.");
     }
 
 }
